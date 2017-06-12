da06009ba796d7b9426a5a9465190fc9415e1e74
yama-natuki <yama.natuki@gmail.com>
2017-06-12 17:39:43 +0900

    Remove:  簡単なコマンドラインエラー処理を削除。

--- siroieiyu2aozora.pl
+++ siroieiyu2aozora.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# last updated : 2017/06/12 17:34:58 JST
+# last updated : 2017/06/12 17:39:16 JST
 #
 # 白衣の英雄を 取得して青空文庫形式に変換する。
 # 512kbごとにファイルを分割して保存します。
@@ -169,11 +169,8 @@ sub get_write_all {
   if ($ARGV[0] =~ m|https?://nemuiyon.blog72.fc2.com/|){
   	print &get_book( $ARGV[0]);
   	exit 0;
-  } else {
-	print "URLが間違っています。\n";
-	exit 0;
   }
-  
+
   if ( $bunkatu == 0 ){
 	print $header;
 	&get_all;
